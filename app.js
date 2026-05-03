5e2f247d6d63599eadcf6c54333c85d8e05b59e1
tobinguyenx <nguyentaitino@gmail.com>
2026-05-03 19:27:18 +1000

    update foods

---
+++ app.js
@@ -0,0 +1,52 @@
+const foods = [
+  {
+    name: "Pho with Sliced Beef",
+    price: 17,
+  },
+  {
+    name: "Bún Bò Huế - Spicy Beef Soup",
+    price: 17.5,
+  },
+  {
+    name: "Pho with Sliced Beef and well done Flank",
+    price: 17,
+  },
+  {
+    name: "Pho with Sliced Beef, Tripe and well done Flank",
+    price: 17,
+  },
+  {
+    name: "Pho with Sliced Beef, Well done Flank and Soft Tendon",
+    price: 17,
+  },
+  {
+    name: "Pho with Beef Balls",
+    price: 17,
+  },
+  {
+    name: "Pho with Special Beef Combination",
+    price: 18,
+  },
+  {
+    name: "Pho with Special Chicken Combination",
+    price: 18,
+  },
+  {
+    name: "Pho with Beef and Chicken",
+    price: 18,
+  },
+  {
+    name: "Pho with Chicken",
+    price: 17,
+  },
+  {
+    name: "Pho with Beef and Beef Balls",
+    price: 17,
+  },
+  {
+    name: "Pho with Special Beef and Chicken Combination",
+    price: 19,
+  },
+];
+
+const menu = document.getElementById("menu");
